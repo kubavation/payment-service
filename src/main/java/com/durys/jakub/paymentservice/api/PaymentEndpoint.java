bfcea3f0f20f805b7bead9727b999ad8eae49061
kubavation <37585627+kubavation@users.noreply.github.com>
2023-09-15 21:24:30 +0200

    fix

--- src/main/java/com/durys/jakub/paymentservice/api/PaymentEndpoint.java
+++ src/main/java/com/durys/jakub/paymentservice/api/PaymentEndpoint.java
@@ -2,6 +2,7 @@ package com.durys.jakub.paymentservice.api;
 
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import org.webservicesoap.payments.GetPaymentRequest;
 import org.webservicesoap.payments.GetPaymentResponse;
@@ -13,7 +14,7 @@ public class PaymentEndpoint {
 
     @PayloadRoot(localPart = "getPaymentRequest", namespace = "http://www.webservicesoap.org/payments")
     @ResponsePayload
-    public GetPaymentResponse payments(GetPaymentRequest request) {
+    public GetPaymentResponse payments(@RequestPayload GetPaymentRequest request) {
         var response = new GetPaymentResponse();
         response.setAmount(BigDecimal.valueOf(1000));
         response.setType("Payment 1");
